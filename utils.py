374e33e4296b0999af4b5290b2c1b13bd5871025
yehzhang <simonzhangintp@gmail.com>
2016-05-13 03:09:58 -0700

    Add utility functions

--- utils.py
+++ utils.py
@@ -13,8 +13,14 @@ def check_dir(directory):
         if not input(msg).startswith('y'):
             sys.exit(0)
 
-def write_to_files(self, path, name, text):
+def write_to_files(path, name, text):
     os.makedirs(path, exist_ok=True)
     path = os.path.join(path, name)
     with open(path, 'w') as fout:
         fout.write(text)
+
+def align(text, offset=0, mark='-'):
+    left, right = zip(*[(l[:offset], l[offset:]) for l in text.split('\n')])
+    lines = [(l, l.find(mark)) for l in right]
+    _, imk = max(lines, key=lambda x: x[1])
+    return '\n'.join(h + (l[:i] + ' ' * (imk - i) + l[i:] if i != -1 else l) for h, (l, i) in zip(left, lines))
